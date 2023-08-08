b32ac86e1a696664949990c8cd7a4896e11a3733
Alexander Korobov <dktwho@mail.ru>
2023-08-08 20:10:13 +0300

    clear unusable import

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent} from 'react';
+import React from 'react';
 import {FilterValuesType} from './App';
 import {AddItemForm} from './AddItemForm';
 import {EditableSpan} from './EditableSpan';
