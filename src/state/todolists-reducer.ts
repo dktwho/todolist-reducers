9f7c06d0aa24ccb1671c5f5deee1be9513ae6953
Alexander Korobov <dktwho@mail.ru>
2023-08-08 18:56:37 +0300

    create addTodolistAC -action creator with typed and  unit test

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -1,10 +1,16 @@
 import {TodolistType} from "../App";
+import {v1} from "uuid";
 
 export  const todolistsReducer = (state: TodolistType[], action: AllActionsType) => {
     switch (action.type) {
         case 'REMOVE-TODOLIST': {
             return state.filter(tl => tl.id !== action.payload.id)
         }
+
+        case 'ADD-TODOLIST': {
+            return [...state, {id: v1(), title: action.payload.title, filter: 'all'}]
+        }
+
         default: {
             return state
         }
@@ -12,8 +18,13 @@ export  const todolistsReducer = (state: TodolistType[], action: AllActionsType)
 }
 
 
-type AllActionsType = removeTodolistACType
+type AllActionsType = removeTodolistACType | addTodolistACType
 type removeTodolistACType = ReturnType<typeof removeTodolistAC >
 export const removeTodolistAC = (id: string) => {
     return {type: 'REMOVE-TODOLIST', payload: {id} } as const
+}
+
+export type addTodolistACType = ReturnType<typeof addTodolistAC >
+export const addTodolistAC = (title: string) => {
+    return {type: 'ADD-TODOLIST', payload: {title} } as const
 }
